fa51885611cea868ce9879f85689c959192369c2
jackdang <jack@Jacks-MacBook-Pro.local>
2017-02-07 18:04:31 +1100

    master

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-    setInterval(function(){
-        var currentImage = $("#slider .current");
-        var isLast = $(currentImage).hasClass("last");
-        if(isLast === true) {
-            var nextImage = $("#slider .first");
+    $("#m_nav_trigger").click(function(){
+        $(".overlay").toggleClass("active"); 
+    });
+    $(".closebtn").click(function(e){
+        e.preventDefault();
+        $(".overlay").removeClass("active");
+    });
+    $(".swipebox-video").swipebox();
+    $('.owl-carousel').owlCarousel({
+    center: false,
+    items:6,
+    loop:false,
+    margin:0,
+    responsive:{
+        600:{
+            items:5
         }
-        else {
-            var nextImage = $(currentImage).next();
-        }
-        $(currentImage).removeClass("current");
-        $(nextImage).addClass("current");
-    }, 5000);
+    }
+});
+    $(".desktop_menu ul li a").click(function(){
+        var goTo = $($(this).attr('href'));
+        var offset = goTo.offset();
+        $("body").animate({scrollTop: offset.top}, 1000);
+    });
 });
